ccc9d29b1692bb0016b2438ba26e5962001a74a6
Jph Orq <josephorquita@gmail.com>
2026-09-05 02:44:54 +0800

    update all

--- js/script.js
+++ js/script.js
@@ -161,9 +161,3 @@ projects.forEach((project) => {
 
   projectGrid.appendChild(article);
 });
-
-// Get the current year
-const currentYear = new Date().getFullYear();
-
-// Update the content of the element with id="currentYear"
-document.getElementById("currentYear").textContent = currentYear;
